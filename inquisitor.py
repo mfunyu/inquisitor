d34e6499ed55e2ed06fe563b76dbab325998dedc
mfunyu <mfunyu@student.42lyon.fr>
2023-12-14 16:19:56 +0100

    add: ip validation

--- inquisitor.py
+++ inquisitor.py
@@ -1,6 +1,35 @@
 #!/usr/bin/python3
 import argparse
 
+def error_exit(msg):
+	print(f"Error: {msg}")
+	exit(1)
+
+def is_valid_ip(ip):
+	try:
+		nums = ip.split('.')
+		if len(nums) != 4:
+			return False
+		for n in nums:
+			if int(n) < 0 or 255 < int(n):
+				return False
+		return True
+	except:
+		return False
+
+def is_valid_mac(mac):
+	return True
+
+def validate_args(args):
+	if not is_valid_ip(args.ip_src):
+		error_exit("Invalid IP-src")
+	if not is_valid_mac(args.mac_src):
+		error_exit("Invalid MAC-src")
+	if not is_valid_ip(args.ip_target):
+		error_exit("Invalid IP-target")
+	if not is_valid_mac(args.mac_target):
+		error_exit("Invalid MAC-target")
+
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("ip_src", type=str, help="IP-src")
@@ -13,6 +42,7 @@ def parse_args():
 
 def main():
 	args = parse_args()
+	validate_args(args)
 	print(args)
 
 if __name__ == '__main__':
